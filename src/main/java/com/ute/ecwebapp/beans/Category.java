73aba744f8019ec845ee48eec0b54b0654ec019c
Khoa Ngô <nndkhoa9@gmail.com>
2021-11-15 08:29:58 +0700

    Initial commit

---
+++ src/main/java/com/ute/ecwebapp/beans/Category.java
@@ -0,0 +1,27 @@
+package com.ute.ecwebapp.beans;
+
+public class Category {
+  private int CatID;
+  private String CatName;
+
+  public Category() {
+  }
+
+  public Category(int catID, String catName) {
+    this.CatID = catID;
+    this.CatName = catName;
+  }
+
+  public Category(String catName) {
+    this.CatID = -1;
+    this.CatName = catName;
+  }
+
+  public int getCatID() {
+    return CatID;
+  }
+
+  public String getCatName() {
+    return CatName;
+  }
+}
